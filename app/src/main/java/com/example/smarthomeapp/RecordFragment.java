68257fc7e49dbc2e3a4d96a019e4011ac4233a33
Chinhalex <81760712+Chinhalex@users.noreply.github.com>
2022-05-26 00:22:12 +0700

    26052022

--- app/src/main/java/com/example/smarthomeapp/RecordFragment.java
+++ app/src/main/java/com/example/smarthomeapp/RecordFragment.java
@@ -20,6 +20,8 @@ import androidx.fragment.app.Fragment;
 import com.example.smarthomeapp.Module.DeviceModel;
 import com.example.smarthomeapp.databinding.FragmentHomeBinding;
 import com.example.smarthomeapp.databinding.FragmentRecordBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -34,6 +36,7 @@ public class RecordFragment extends Fragment {
     protected static final int RESULT_SPEECH = 1;
     private  List<DeviceModel> deviceModels;
     private FirebaseDatabase firebaseDatabase;
+    FirebaseAuth mAuth;
 
 
 
@@ -43,6 +46,9 @@ public class RecordFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentRecordBinding.inflate(inflater,container,false);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
         binding.btnSpeak.setOnClickListener(view1 -> {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -58,7 +64,8 @@ public class RecordFragment extends Fragment {
 
 
         deviceModels = new ArrayList<>();
-        firebaseDatabase.getInstance().getReference("livingroom")
+        firebaseDatabase.getInstance().getReference("Users")
+                .child(firebaseUser.getUid()).child("device")
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -85,6 +92,9 @@ public class RecordFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+
         switch (requestCode){
             case RESULT_SPEECH:
                 if(resultCode == RESULT_OK && data != null){
@@ -95,13 +105,15 @@ public class RecordFragment extends Fragment {
                             //+ deviceModels.get(i).getName()
                             if(dataVoice.contentEquals("Turn on "+ deviceModels.get(i).getName())||dataVoice.contentEquals("turn on "+ deviceModels.get(i).getName())){
                                 deviceModels.get(i).status = true;
-                                firebaseDatabase.getInstance().getReference("livingroom")
+                                firebaseDatabase.getInstance().getReference("Users")
+                                        .child(firebaseUser.getUid()).child("device")
                                         .child(deviceModels.get(i).getId()).setValue(deviceModels.get(i));
                                 Toast.makeText(requireContext(), "đã bật "+ deviceModels.get(i).getName(), Toast.LENGTH_LONG).show();
                             }
                             else if(dataVoice.contentEquals("Turn off "+ deviceModels.get(i).getName())||dataVoice.contentEquals("turn off "+ deviceModels.get(i).getName())){
                                 deviceModels.get(i).status = false;
-                                firebaseDatabase.getInstance().getReference("livingroom")
+                                firebaseDatabase.getInstance().getReference("Users")
+                                        .child(firebaseUser.getUid()).child("device")
                                         .child(deviceModels.get(i).getId()).setValue(deviceModels.get(i));
                                 Toast.makeText(requireContext(), "đã tắt "+ deviceModels.get(i).getName(), Toast.LENGTH_LONG).show();
                             }
